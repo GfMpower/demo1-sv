b40b0667da0be4690e40554e193076725db51a7a
GfMpower <GfMpower@itcast.cn>
2026-01-31 15:02:05 +0800

    Vue 3项目结构解析

--- demo-vue/src/main.js
+++ demo-vue/src/main.js
@@ -1,12 +1,18 @@
+//导入Vue的createApp的函数，用于创建应用程序
 import { createApp } from 'vue'
+//导入根组件App.vue
 import App from './App.vue'
+//导入路由配置
 import router from './router'
+//导入Element Plus-UI的组件
 import ElementPlus from 'element-plus'
+//导入Element Plus-UI的样式文件
 import 'element-plus/dist/index.css'
-
+//使用根组件App创建Vue应用程序
 const app = createApp(App)
-
+//使用路由插件
 app.use(router)
+//使用Element Plus-UI组件库
 app.use(ElementPlus)
-
+//挂载应用程序到http中id为app的DOM元素上
 app.mount('#app')
--- demo-vue/vite.config.js
+++ demo-vue/vite.config.js
@@ -1,11 +1,27 @@
+//导入Node.js中的fileURLToPath和URL模块相功能，用于处理文件路径
 import { fileURLToPath, URL } from 'node:url'
-
+//导入Vite的defineConfig函数，用于创建Vite配置对象
 import { defineConfig } from 'vite'
+//导入Vue插件
 import vue from '@vitejs/plugin-vue'
+//导入Vue开发工具插件
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //开发服务器配置
+  server: {
+    port: 8081,
+    host: 'localhost', //主机地址
+    open: true, //自动打开浏览器
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', //后端api地址
+        changeOrigin: true, //是否改变源地址
+        rewrite: (path) => path.replace(/^\/api/, '') //重写路径
+      }
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
